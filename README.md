4d731b425d972478a9129d370ea1e669b0dadac8
SeaLabEZF <sean.wulwick@gmail.com>
2018-04-23 18:39:06 -0700

    removed unneeded files

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# cpsc353
+Name: Sean Wulwick
+
+		project.py
+
+SYNOPSIS
+
+	python3 project.py [-h|-H]
+
+	python3 project.py [-d|-D] [image] [output-file]
+
+	python3 project.py [-e|-E] [image] [input-file] [output-image]
+
+
+DESCRIPTION
+
+	project.py is used to ether encode or decode messages that have been stored
+
+	within an image. The message, if decoded, message will be placed in your output
+
+	file. If encode is used the message must be placed in a text file and fed as an argument.
+
+
+COMMAND
+
+	[-h|-H] --help		displays a short description of what the program does and how to run it.
+
+	[-d|-D] --decode	takes <*.png> image and a name for your output message.
+
+	[-e|-E] --encode	takes <*.jpg> image and a <*.txt> file as input and takes a name for your output image.
+
+Sample input:
+
+python3 project.py -e @w3$0m31m@g3.jpg hackingscript.txt sUpperSeecreet.png
+
+python3 project.py -d sUpperSeecreet.png seecreeet.txt
+
+python3 project.py -h
--- project.py
+++ project.py
@@ -3,6 +3,48 @@ import math
 import sys
 
 ######################################################
+# Function: Main                                     #
+######################################################
+def main():
+    if len(sys.argv) == 1:
+        print("missing parameter (can has [-h|-H|-d|-D|-e|-E]?)")
+    elif len(sys.argv) == 2 and (sys.argv[1] == '-h' or sys.argv[1] == '-H'):
+        helpMenu()
+    elif len(sys.argv) == 4 and (sys.argv[1] == '-d' or sys.argv[1] == '-D'):
+        inImage = sys.argv[2]
+        infile = sys.argv[3]
+        if inImage[-4:] == ".png" and infile[-4:] == ".txt":
+            img = Image.open(inImage)
+            msg = decode(img)
+            file = open(infile, "w")
+            file.write(msg)
+            file.close()
+            img.close()
+            print("Encoded message stored in", infile)
+        else:
+            print("issue with last two arguments, check file extension (*.png , *.txt)")
+    elif len(sys.argv) == 5 and (sys.argv[1] == '-e' or sys.argv[1] == '-E'):
+        inImage = sys.argv[2]
+        infile = sys.argv[3]
+        outImage = sys.argv[4]
+        if inImage[-4:] == ".jpg" and infile[-4:] == ".txt":
+            if outImage[-4:] == ".png":
+                outImage = outImage[:-4]
+            outImage = outImage + ".png"
+            file = open(infile)
+            msg = file.read()
+            file.close()
+            img = Image.open(inImage)
+            encode(img, msg)
+            img.save(outImage, "PNG")
+        else:
+            print("issue with last three arguments, check file extensions (*.jpg , *.txt , *)")
+    elif len(sys.argv) > 5:
+        print("Too many arguments, what are you doing?! STAHP!!!!")
+    else:
+        print("Check parameters (python3 project.py -h)")
+
+######################################################
 # Function: decode                                   #
 # Input: img                                         #
 # Output: converted_msg                              #
@@ -21,20 +63,20 @@ def decode(img):
     col = width-1
     row = height-1
 
-    msg_size = extractor(width, col, row)
+    msg_size = extractor(img, width, col, row)
     msg_size = msg_size[:32]
     msg_size = int(msg_size,2)
 
     col = width-12
     pixel_amount = math.ceil(msg_size/3)
-    msg = extractor(width, col, row, pixel_amount)
+    msg = extractor(img, width, col, row, pixel_amount)
     msg = msg[:msg_size]
     converted_msg = ''.join(chr(int(msg[i:i+8],2)) for i in range(0, len(msg),8))
     return converted_msg
 
 ######################################################
 # Function: extractor                                #
-# Input: width, col, row, finish(default = 11)       #
+# Input: img, width, col, row, finish(default = 11)  #
 # Output: information_bin                            #
 #                                                    #
 # Description:                                       #
@@ -44,7 +86,7 @@ def decode(img):
 # getpixel function to access RGB, reads the LSB     #
 # and stores it within information_bin as a string   #
 ######################################################
-def extractor(width, col, row, finish = 11):
+def extractor(img, width, col, row, finish = 11):
 
     information_bin = ''
     for i in range(finish):
@@ -81,24 +123,22 @@ def encode(img, msg):
     msg_len_bin = msg_len_bin[2:]
     msg_len_bin = ("0" * 32) + msg_len_bin + "0"
     msg_len_bin = msg_len_bin[-33:]
-    print(msg_len_bin)
 
     msg_bin = str(bin(int.from_bytes(msg.encode(), 'big')))
     msg_bin = msg_bin[:1] + msg_bin[2:]
 
     pixel_amount = int(msg_len/3)
-    print(pixel_amount)
     print("injecting size")
-    injector(width, col, row, 11, msg_len_bin)
+    injector(img, width, col, row, 11, msg_len_bin)
     print("size injected")
     col = width-12
     print("injecting message")
-    injector(width, col, row, pixel_amount, msg_bin)
+    injector(img, width, col, row, pixel_amount, msg_bin)
     print("message injected")
     
 ######################################################
 # Function: injector                                 #
-# Input: width, col, row, finish, bin_file           #
+# Input: img, width, col, row, finish, bin_file      #
 # Output: N/A                                        #
 #                                                    #
 # Description:                                       #
@@ -109,7 +149,7 @@ def encode(img, msg):
 # them with the string of bits from bin_file by      #
 # by calling the newRGB function                     #
 ######################################################
-def injector(width, col, row, finish, bin_file):
+def injector(img, width, col, row, finish, bin_file):
     bin_file_index = 0
     for i in range(finish):
         r, g, b = img.getpixel((col, row))
@@ -143,16 +183,15 @@ def newRGB(originalRGB, bit):
         return originalRGB + bit
 
 ######################################################
-# main                                               #
-# Input: argv[n]                                     #
+# Function: helpMenu                                 #
+# Input: N/A                                         #
 # Output: N/A                                        #
 #                                                    #
 # Description:                                       #
-# where all the action happens.                      #
+# Outputs a help menu for the program.               #
 ######################################################
-if len(sys.argv) == 1:
-    print("missing parameter (can has [-h|-H|-d|-D|-e|-E]?)")
-elif len(sys.argv) == 2 and (sys.argv[1] == '-h' or sys.argv[1] == '-H'):
+
+def helpMenu():
     print("\t\tproject.py\n")
     print("SYNOPSIS\n")
     print("\tpython3 project.py [-h|-H]\n")
@@ -160,43 +199,12 @@ elif len(sys.argv) == 2 and (sys.argv[1] == '-h' or sys.argv[1] == '-H'):
     print("\tpython3 project.py [-e|-E] [image] [input-file] [output-image]\n")
     print("\nDESCRIPTION\n")
     print("\tproject.py is used to ether encode or decode messages that have been stored\n")
-    print("\twithin an image. The message, if decoded, will be displayed in the terminal\n")
-    print("\tand outputed to a file. If encode is used the message must be placed in a text\n")
-    print("\tfile and fed as an argument.")
+    print("\twithin an image. The message, if decoded, message will be placed in your output\n")
+    print("\tfile. If encode is used the message must be placed in a text file and fed as an argument.\n")
     print("\nCOMMAND\n")
     print("\t[-h|-H] --help\t\tdisplays a short description of what the program does and how to run it.\n")
     print("\t[-d|-D] --decode\ttakes <*.png> image and a name for your output message.\n")
     print("\t[-e|-E] --encode\ttakes <*.jpg> image and a <*.txt> file as input and takes a name for your output image.\n")
-elif len(sys.argv) == 4 and (sys.argv[1] == '-d' or sys.argv[1] == '-D'):
-    infile = sys.argv[3]
-    inImage = sys.argv[2]
-    if inImage[-4:] == ".png" and infile[-4:] == ".txt":
-        img = Image.open(inImage)
-        msg = decode(img)
-        file = open(infile, "w")
-        file.write(msg)
-        file.close()
-        img.close()
-        print(msg)
-    else:
-        print("issue with last two arguments, check file extension (*.png , *.txt)")
-elif len(sys.argv) == 5 and (sys.argv[1] == '-e' or sys.argv[1] == '-E'):
-    inImage = sys.argv[2]
-    infile = sys.argv[3]
-    outImage = sys.argv[4]
-    if inImage[-4:] == ".jpg" and infile[-4:] == ".txt":
-        if outImage[-4:] == ".png":
-            outImage = outImage[:-4]
-        outImage = outImage + ".png"
-        file = open(infile)
-        msg = file.read()
-        file.close()
-        img = Image.open(inImage)
-        encode(img, msg)
-        img.save(outImage, "PNG")
-    else:
-        print("issue with last three arguments, check file extensions (*.jpg , *.txt , *)")
-elif len(sys.argv) > 5:
-    print("Too many arguments, what are you doing?! STAHP!!!!")
-else:
-    print("Check your parameter's!")
+
+if __name__ == '__main__':
+    main()
